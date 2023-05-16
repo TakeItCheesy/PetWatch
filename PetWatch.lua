bb31b09c688e60434fbc52c5046636ba0c715271
TakeItCheesy <132988099+TakeItCheesy@users.noreply.github.com>
2023-05-16 12:50:26 +0200

    Update PetWatch.lua

--- PetWatch.lua
+++ PetWatch.lua
@@ -36,18 +36,33 @@ config = require ('config')
 texts  = require('texts')
 packets = require('packets')
 require('tables')
+require('sets')
 
 function init()
+	PetName = nil
+	PetIndex = nil
+	CurrentHP = 0
+	MaxHP = 0
+	CurrentMP = 0
+	MaxMP = 0
+	HPP = 0
+	MPP = 0
+	TP = 0
+	PetActive = false
+
 	Defaults = T{	
-	pos = { x=0, y=0 },
+	pos = { 
+		x = windower.get_windower_settings().x_res*2/3
+		y = windower.get_windower_settings().y_res-20
+	},
 	padding = 5,
 	text = { size=12, font='Impact' }
-	}
+	}	
 	
 	Settings = config.load('data\\settings.xml', Defaults)	
+	
 	PetWatch = texts.new()
 	PetWatch:bg_alpha(150)	
-	PetWatch:bg_color(40, 40, 55)
 	PetWatch:font(Settings.text.font)
 	PetWatch:size(Settings.text.size)
 	PetWatch:pos(Settings.pos.x, Settings.pos.y)
@@ -55,18 +70,58 @@ function init()
 end
 
 function show()
-
+	PetActive = true
+    PetWatch:show()
 end
 
 function hide()
-
+	PetActive = false
+	PetName = nil
+	PetIndex = nil
+	CurrentHP = 0
+	MaxHP = 0
+	CurrentMP = 0
+	MaxMP = 0
+	HPP = 0
+	MPP = 0
+	TP = 0
+    PetWatch:hide()
 end
 
 function update()
-	local pet = windower.ffxi.get_mob_by_target('pet')
+	if not S{9, 14, 15, 18, 21}:contains(windower.ffxi.get_player()['main_job_id']) then 
+		hide()
+		return 
+	end
 	
+	local pet = windower.ffxi.get_mob_by_target('pet') or nil
+	
+	if pet then
+		PetActive = true
+		PetName = pet.name
+		PetIndex = pet.index
+		HPP = pet.hpp
+		MPP = pet.mpp
+		TP = pet.tp		
+	end
+	
+	    -- if HPP > 74 then
+            -- color = {128, 255, 0}
+			-- flavor = 'feelin good (^_^)'
+        -- elseif HPP > 49 then
+            -- color = {255, 255, 0}
+			-- flavor = 'taking hits (._.)'
+        -- elseif HPP > 24 then
+            -- color = {255, 150, 0}
+			-- flavor = 'halp! (@_@)'
+        -- elseif HPP > 0 then
+            -- color = {255, 0, 0}
+			-- flavor = 'healz plx! (>.<)'
+		-- else
+			-- color = {150, 0, 0}
+			-- flavor = 'ded! (x.x)'
+        -- end
 end
-
 
 handle_commands = function(...)
 	local args = T{...}		
@@ -75,9 +130,11 @@ handle_commands = function(...)
 		if cmd == 'reset' then
 			
 		end
+		if cmd == 'pos' then
+		
+		end
 	end
 end
-
 
 windower.register_event('addon command', handle_commands)
 
